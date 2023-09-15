17e34d6c64b6e4fcd4ff721f4e5657022922266b
ynskrca12 <ynskrca52@outlook.com>
2023-09-15 23:59:23 +0300

    Display all Invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    //
+    public function get_all_invoice(){
+        $invoices = Invoice::with('customer')->orderBy('id','DESC')->get();
+        return response()->json([
+            'invoices' => $invoices
+        ],200);
+    }
 }
