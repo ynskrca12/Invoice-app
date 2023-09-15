f53fef322ad5e54e62e674997f19a63272575d09
ynskrca12 <ynskrca52@outlook.com>
2023-09-16 02:31:13 +0300

    Search Invoice

--- resources/js/components/invoices/index.vue
+++ resources/js/components/invoices/index.vue
@@ -2,6 +2,7 @@
     import { onMounted,ref } from 'vue';
 
     let invoices = ref([])
+    let searchInvoice = ref([])
 
     onMounted(async () => {
         getInvoices()
@@ -10,6 +11,11 @@
     const getInvoices = async () => {
         let response = await axios.get("/api/get_all_invoice")
         // console.log('response',response)
+        invoices.value = response.data.invoices
+    }
+
+    const search = async () => {
+        let response = await axios.get('/api/search_invoice?s='+searchInvoice.value)
         invoices.value = response.data.invoices
     }
 </script>
@@ -58,7 +64,8 @@
                 </div>
                 <div class="relative">
                     <i class="table--search--input--icon fas fa-search "></i>
-                    <input class="table--search--input" type="text" placeholder="Search invoice">
+                    <input class="table--search--input" type="text" placeholder="Search invoice"
+                    v-model = "searchInvoice" @keyup="search()">
                 </div>
             </div>
 
--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/get_all_invoice',[InvoiceController::class, 'get_all_invoice']);
+Route::get('/search_invoice',[InvoiceController::class, 'search_invoice']);
